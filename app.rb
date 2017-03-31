36856bc154368624614f58f83bf2b01dd5607d15
eaglesant <eagle_21@mail.ru>
2017-04-01 00:26:14 +0300

    Change visit form and add users.txt

--- app.rb
+++ app.rb
@@ -16,3 +16,22 @@ end
 get '/visit' do
 	erb :visit
 end
+post '/visit' do
+	@user_name = params[:user_name]
+	@user_email = params[:user_email]
+	@user_phone = params[:user_phone]
+	@date_time = params[:date_time]
+	@barber = params[:barber]
+
+	@title = 'Thank you!'
+
+	@message = "Dear #{@user_name}, we'll waiting for you at #{@date_time} with #{@barber}"
+
+	f = File.open "./public/users.txt", "a"
+
+	f.write "User: #{@user_name}, phone: #{@user_phone}, Email: #{@user_email}, date and time: #{@date_time}, barber: #{@barber}"
+	f.close
+
+	erb :message
+
+end
