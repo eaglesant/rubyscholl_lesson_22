03831b7d94bd60526dd45c459e14db6617cdd3f3
eaglesant <eagle_21@mail.ru>
2017-03-31 22:29:29 +0300

    Change title and name site

--- app.rb
+++ app.rb
@@ -6,7 +6,3 @@ require 'sinatra/reloader'
 get '/' do
 	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
 end
-
-get '/contacts' do
-	erb :contacts
-end
